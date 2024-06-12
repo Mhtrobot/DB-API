23dba2a04ad7cc44df1748614c8bde508f5a953c
Mhtrobot <mhtrobot760@gmail.com>
2024-06-12 16:54:43 +0330

    db register user finish

--- main.py
+++ main.py
@@ -19,3 +19,15 @@ def read_root():
 @app.get('/users')
 def read_users(limit: int, db: Session = Depends(get_db)):
     return db.query(models.USER).limit(limit).all()
+
+@app.post('/register/user')
+def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
+    isTaken = db.query(models.USER).filter(models.USER.phone == user.phone).first()
+    if isTaken:
+        raise HTTPException(status_code=404, detail=f"User with phone {user.phone} already exists")
+
+    db_user = models.USER(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
