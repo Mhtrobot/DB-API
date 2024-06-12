a08689e84369606f0f59fbe49cc8d50dbecda0ad
Mhtrobot <mhtrobot760@gmail.com>
2024-06-12 20:17:40 +0330

    user update finish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
-
 from fastapi import FastAPI, Depends, HTTPException,status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from typing import Annotated, List
 from sql import models, schemas, auth
 from sql.database import SessionLocal, engine
 
@@ -14,6 +14,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_dependency = Annotated[SessionLocal, Depends(get_db)]
 
 @app.get('/')
 def read_root():
@@ -52,3 +54,19 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         "message": "Login Successful",
         "user_detail": user,
         "access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/current", response_model=schemas.UserBase)
+def get_current_user(current_user: models.USER = Depends(auth.get_current_user)):
+    return current_user
+
+@app.put('/user-update/${user_id}', response_model=schemas.UserUpdate)
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = db.query(models.USER).filter(models.USER.user_id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="User not found")
+    for key, value in user.dict(exclude_unset=True).items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
